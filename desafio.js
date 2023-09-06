ccf592b51bc2cad3e63875d7639c8e443a298ed6
iajor <rjai.neto@gmail.com>
2023-09-06 16:32:14 -0300

    desafio

--- desafio.js
+++ desafio.js
@@ -5,40 +5,40 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function obterNivelEExibirMensagem() {
-  rl.question("Digite o nome do herói (ou 'sair' para encerrar): ", (nomeDoHeroi) => {
+function informarNomeENivel() {
+  rl.question("Informe o nome do herói (ou 'sair' para terminar): ", (nomeDoHeroi) => {
     if (nomeDoHeroi.toLowerCase() === 'sair') {
       rl.close(); 
     } else {
-      rl.question("Digite a quantidade de experiência (XP) do herói: ", (xpInput) => {
-        const xpDoHeroi = parseFloat(xpInput);
+      rl.question("Informe a quantidade de experiência do herói: ", (expInput) => {
+        const expDoHeroi = parseFloat(expInput);
 
         let nivelDoHeroi;
 
-        if (xpDoHeroi < 1000) {
+        if (expDoHeroi < 1000) {
           nivelDoHeroi = "Ferro";
-        } else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+        } else if (expDoHeroi >= 1001 && expDoHeroi <= 2000) {
           nivelDoHeroi = "Bronze";
-        } else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+        } else if (expDoHeroi >= 2001 && expDoHeroi <= 5000) {
           nivelDoHeroi = "Prata";
-        } else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+        } else if (expDoHeroi >= 6001 && expDoHeroi <= 7000) {
           nivelDoHeroi = "Ouro";
-        } else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+        } else if (expDoHeroi >= 7001 && expDoHeroi <= 8000) {
           nivelDoHeroi = "Platina";
-        } else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+        } else if (expDoHeroi >= 8001 && expDoHeroi <= 9000) {
           nivelDoHeroi = "Ascendente";
-        } else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+        } else if (expDoHeroi >= 9001 && expDoHeroi <= 10000) {
           nivelDoHeroi = "Imortal";
         } else {
           nivelDoHeroi = "Radiante";
         }
 
-        console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
+        console.log(`O Herói ${nomeDoHeroi} está no nível ${nivelDoHeroi}`);
 
-        obterNivelEExibirMensagem();
+        informarNomeENivel();
       });
     }
   });
 }
 
-obterNivelEExibirMensagem();
+informarNomeENivel();
