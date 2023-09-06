c3d82024303640ba04096d661596bc8859729d5a
iajor <rjai.neto@gmail.com>
2023-09-06 16:15:40 -0300

    desafio

--- desafio.js
+++ desafio.js
@@ -5,34 +5,44 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("Digite o nome do herói: ", (nomeDoHeroi) => {
-  rl.question("Digite a quantidade de experiência (XP) do herói: ", (xpInput) => {
-    // Converte a entrada do usuário em um número
-    const xpDoHeroi = parseFloat(xpInput);
-
-    let nivelDoHeroi;
-
-    if (xpDoHeroi < 1000) {
-      nivelDoHeroi = "Ferro";
-    } else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
-      nivelDoHeroi = "Bronze";
-    } else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
-      nivelDoHeroi = "Prata";
-    } else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
-      nivelDoHeroi = "Ouro";
-    } else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
-      nivelDoHeroi = "Platina";
-    } else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
-      nivelDoHeroi = "Ascendente";
-    } else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
-      nivelDoHeroi = "Imortal";
-    } else {
-      nivelDoHeroi = "Radiante";
+function obterNivelEExibirMensagem() {
+  rl.question("Digite o nome do herói (ou 'sair' para encerrar): ", (nomeDoHeroi) => {
+    if (nomeDoHeroi.toLowerCase() === 'sair') {
+      rl.close(); 
+      return;
     }
 
-    // Exibe a mensagem final
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
+    rl.question("Digite a quantidade de experiência (XP) do herói: ", (xpInput) => {
+      const xpDoHeroi = parseFloat(xpInput);
 
-    rl.close();
+      let nivelDoHeroi;
+
+      if (xpDoHeroi < 1000) {
+        nivelDoHeroi = "Ferro";
+      } else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+        nivelDoHeroi = "Bronze";
+      } else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+        nivelDoHeroi = "Prata";
+      } else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+        nivelDoHeroi = "Ouro";
+      } else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+        nivelDoHeroi = "Platina";
+      } else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+        nivelDoHeroi = "Ascendente";
+      } else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+        nivelDoHeroi = "Imortal";
+      } else {
+        nivelDoHeroi = "Radiante";
+      }
+
+      
+      console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
+
+      
+      obterNivelEExibirMensagem();
+    });
   });
-});
+}
+
+
+obterNivelEExibirMensagem();
